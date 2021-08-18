90fb663f9dad34550aee06e284d8bcf921c9edab
krutov-sergey <krus@mts.ru>
2021-08-18 18:32:51 +0500

    калькулятор готов

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,31 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+const Root = defineComponent({
+    name: 'Root',
+    data() {
+        return {
+            a: 2,
+            b: 3,
+            radio: 'multiply',
+        };
+    },
+    computed: {
+        result: function() {
+            switch (this.radio){
+                case 'sum':
+                    return this.a + this.b;
+                case 'subtract':
+                    return this.a - this.b;
+                case 'multiply':
+                    return this.a * this.b;
+                case 'divide':
+                    return this.a / this.b;
+                default:
+                    return 'Ошибка вычисления!';
+            }
+        }
+    },
+});
+
+const app = createApp(Root).mount('#app');
